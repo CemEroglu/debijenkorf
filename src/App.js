094c7a7a50f81ea8cb527ce32e5c25fefbae8fa8
TURKCELL\tcceroglu <cem.eroglu@turkcell.com.tr>
2021-07-13 15:16:08 +0300

    width has a problem on 'calvin'

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import SearchBar from './components/SearchBar';
 import data from './data/people.json'
 function App() {
   return (
-    <div className="App">
+    <div>
       <SearchBar lookUpData={data} maxElement={4}></SearchBar>
     </div>
   );
